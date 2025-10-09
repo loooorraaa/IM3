5da0c9f8358acb1e6dcfe3e23be9bff245ed0c4f
Marina Lampert <marina.lampert@stud.fhgr.ch>
2025-10-09 10:18:05 +0200

    update 9.10.

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ fetch('https://interaktivemedien3.laura-seger.ch/php/unload.php')
   .catch(error => {
     console.error('Error fetching exchange rates:', error);
   });
+
+  // Chart.js
+  let myChart = document.querySelector('#myChart');
+  const config = {
+  type: 'line',
+  data: data,
+};
+
+const labels = Utils.months({count: 7});
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'My First Dataset',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    fill: false,
+    borderColor: 'rgb(75, 192, 192)',
+    tension: 0.1
+  }]
+};
